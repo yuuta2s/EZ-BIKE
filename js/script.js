3523eb47a1ea2e545289ed195412f81eb62f1eae
CalliEtc <callipter@gmail.com>
2024-02-20 16:42:54 +0100

    html et css de la page contact et ajout de la nav bar au fichier script

--- js/script.js
+++ js/script.js
@@ -1,37 +1,38 @@
 // Burger menu
-        function burgerMenu() {
-            console.log('burgerMenu')
-            const navbarMobile = document.getElementById('navbar-mobile')
-            navbarMobile.classList.toggle('d-none')
-        }
 
-        function displayHeader() {
-            console.log('displayHeader')
-            const header = document.createElement('header')
-            header.innerHTML = `
-                <header>
-                    <nav>
-                        <img src="../img/assets/ez-bike_logo.svg" id="logo">
-                        <ul class="navbar-desktop">
-                            <li><a href="../index.html">Accueil</a></li>
-                            <li><a href="../PageHtml/modeEmploi.html">Mode d'emploi</a></li>
-                            <li><a href="../PageHtml/maps.html">Trouver un vélo</a></li>
-                            <li><a href="../PageHtml/calculateur.html">Calculateur</a></li>
-                            <li><a href="../PageHtml/contact.html">Contact</a></li>
-                            </ul>
-                            </ul>
-                            </nav>
-                            </header>
-                            `
-            document.body.prepend(header)
-        }
+console.log("Hello");
+function burgerMenu() {
+  console.log("burgerMenu");
+  const navbarMobile = document.getElementById("navbar-mobile");
+  navbarMobile.classList.toggle("d-none");
+}
 
-        // Afficher le footer sur tt les pages
-        function displayFooter() {
-            console.log('displayFooter')
-            const footer = document.createElement('footer')
-            footer.innerHTML = `
+function displayHeader() {
+  console.log("displayHeader");
+  const header = document.createElement("header");
+  header.innerHTML = `
+    <header>
+        <nav>
+            <img src="../img/assets/ez-bike_logo.svg" id="logo">
+            <ul class="navbar-desktop">
+                <li><a href="../index.html">Accueil</a></li>
+                <li><a href="../PageHtml/modeEmploi.html">Mode d'emploi</a></li>
+                <li><a href="../PageHtml/maps.html">Trouver un vélo</a></li>
+                <li><a href="../PageHtml/calculateur.html">Calculateur</a></li>
+                <li><a href="../PageHtml/contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    `;
+  document.body.prepend(header);
+}
+
+// Afficher le footer sur tt les pages
+function displayFooter() {
+  console.log("displayFooter");
+  const footer = document.createElement("footer");
+  footer.innerHTML = `
             <p>&copy; 2024 EZ-BIKE</p>
-            `
-            document.body.append(footer)
-        }
+            `;
+  document.body.append(footer);
+}
