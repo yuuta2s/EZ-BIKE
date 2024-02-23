efbec3542aa50ecc24e621fbb140803515dd8fba
Medou13500 <medoubouzid0@gmail.com>
2024-02-23 13:40:18 +0100

    sauvegarde mode emploi

---
+++ js/modeEmploi.js
@@ -0,0 +1,25 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  const slides = document.querySelectorAll(".carousel .slide");
+  console.log(slides);
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
